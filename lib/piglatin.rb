e906847f7449ec15f0ef2a26daff90b784ba35e9
Catherine O'Neill <cathzozo@gmail.com>
2014-07-28 21:22:04 -0500

    rephrased while loop

--- lib/piglatin.rb
+++ lib/piglatin.rb
@@ -3,19 +3,14 @@ require "piglatin/version"
 module PigLatin
   
   def self.translate(word)
-    
-    word = word.downcase
-    
+    word = word.downcase    
     if word.match(/^[aeiou]/)
       return "#{word}way"
     else
       initial_string = word.slice!(0)
-      while !word.match(/^[aeiouy]/)
-        initial_string = initial_string + word.slice!(0)
-      end
+      initial_string = initial_string + word.slice!(0) while !word.match(/^[aeiouy]/)
       return "#{word}#{initial_string}ay"
     end
-  
   end
 
 end
