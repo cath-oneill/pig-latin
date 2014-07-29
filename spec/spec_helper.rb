168e10a1537f7e2b5ded1df26c0269e8287e188e
Catherine O'Neill <cathzozo@gmail.com>
2014-07-29 10:43:55 -0500

    updated spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
 require 'pry-byebug'
 require 'lib/piglatin.rb'
 
-describe 'PigLatin' do
-  context 'when a word starts with a vowel sound' do
-    it "does not change the beginning of the word, but adds way to the end" do
-      result = PigLatin.translate("apple")
-      expect(result).to eq("appleway")
-    end
-  end
-end
